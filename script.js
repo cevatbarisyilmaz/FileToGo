35412ce3bc11451add3cc0605771c2eb6cc3457b
Cevat Barış Yılmaz <cevatbarisyilmaz@gmail.com>
2019-11-12 15:16:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-let source = document.getElementById("source");
+let sourceFile = document.getElementById("source_file");
+let packageName = document.getElementById("package_name");
+let variableName = document.getElementById("variable_name");
+let array = document.getElementById("array");
+let fileName = document.getElementById("file_name");
+let convertButton = document.getElementById("convert_button");
 
-source.addEventListener("change", handleFiles);
-function handleFiles() {
-	const fileList = this.files;
-	if(fileList.length > 0) {
-		fileList[0].arrayBuffer().then(buffer => {
-			let bytes = new Uint8Array(buffer);
-			let go = "var data = []byte{";
-			go += bytes.join(",");
-			go += "}";
-			let data = new Blob([go], {type: 'text/plain'});
-			let downloadLink = document.createElement("a");
-			downloadLink.href = window.URL.createObjectURL(data);
-			downloadLink.download = "data.go";
-			downloadLink.click();
-			window.URL.revokeObjectURL(downloadLink.href);
-			downloadLink.remove();
-		});
+convertButton.addEventListener("click", () => {
+	if(sourceFile.files.length === 0) {
+		alert("Add A Source File");
+		return;
 	}
-}
+	let pack = packageName.value;
+	if(pack.length === 0) {
+		alert("Enter A Package Name");
+		return;
+	}
+	let variable = variableName.value;
+	if(variable.length === 0) {
+		alert("Enter A Variable Name");
+		return;
+	}
+	let file = fileName.value;
+	if(file.length === 0) {
+		alert("Enter A File Name");
+		return;
+	}
+	sourceFile.files[0].arrayBuffer().then(buffer => {
+		let bytes = new Uint8Array(buffer);
+		let go = "package " + pack + "\n\nvar " + variable + " = [" + (array.checked ? "..." : "") + "]byte{" + bytes.join(",") + "}\n";
+		let data = new Blob([go], {type: 'text/plain'});
+		let downloadLink = document.createElement("a");
+		downloadLink.href = window.URL.createObjectURL(data);
+		downloadLink.download = file + ".go";
+		downloadLink.click();
+		window.URL.revokeObjectURL(downloadLink.href);
+		downloadLink.remove();
+		});
+});
